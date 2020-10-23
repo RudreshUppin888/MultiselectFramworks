73222b0ad04c630f97e5c9e083377e1ed79a14b9
rudresh <rudreshuppinc69@gmail.com>
2020-10-23 21:14:24 +0530

    second

---
+++ MultiSelectFramework/MultiDropDownFramwork.h
@@ -0,0 +1,19 @@
+//
+//  MultiDropDownFramwork.h
+//  MultiDropDownFramwork
+//
+//  Created by Rudresh Uppin on 23/03/20.
+//  Copyright © 2020 Rudresh Uppin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MultiDropDownFramwork.
+FOUNDATION_EXPORT double MultiDropDownFramworkVersionNumber;
+
+//! Project version string for MultiDropDownFramwork.
+FOUNDATION_EXPORT const unsigned char MultiDropDownFramworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MultiDropDownFramwork/PublicHeader.h>
+
+
---
+++ MultiSelectFramework/MultiSelectFramework.h
@@ -0,0 +1,19 @@
+//
+//  MultiSelectFramework.h
+//  MultiSelectFramework
+//
+//  Created by Rudresh Uppin on 27/09/20.
+//  Copyright © 2020 Rudresh Uppin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MultiSelectFramework.
+FOUNDATION_EXPORT double MultiSelectFrameworkVersionNumber;
+
+//! Project version string for MultiSelectFramework.
+FOUNDATION_EXPORT const unsigned char MultiSelectFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MultiSelectFramework/PublicHeader.h>
+
+
